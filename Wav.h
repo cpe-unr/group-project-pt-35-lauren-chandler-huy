d68dbfc5c04250996cc8168a36a64751da75fc3e
huytran <huytrit@gmail.com>
2021-04-24 14:51:23 -0700

    added wav samples and some starting class files

---
+++ Wav.h
@@ -0,0 +1,31 @@
+//Author: Huy Tran, Lauren Feldman
+//Date: 4/20/21
+//Group Project
+
+#ifndef WAV_H
+#define WAV_H
+
+/*
+ * This class is designed to work with PCM 8-bit mono wavefiles.
+ * It makes many assumptions about the format of the wave as a result.
+ * It will NOT work with stereo files or any other bit-depth than 8 bits.
+ */
+#include "WaveHeader.h"
+
+class Wav {
+public:
+    void readFile(const std::string &fileName);
+    void writeFile(const std::string &outFileName);
+private:
+    unsigned char* buffer = NULL;
+    wav_header waveHeader;
+public:
+    virtual ~Wav();
+
+public:
+    unsigned char *getBuffer();
+    int getBufferSize() const;
+};
+
+
+#endif //WAV_H
