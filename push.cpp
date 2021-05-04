7703a9d834fc77f4c87bfdd69fa9f788465f77fd
Lauren Feldman <laurenfeldman@Laurens-MacBook-Air-2.local>
2021-05-04 01:33:57 -0700

    new class in cpp files containing functions that print parameterized vectors

---
+++ push.cpp
@@ -0,0 +1,13 @@
+//Author: Lauren Feldman
+//Date: 4/20/21
+//Group Project
+
+#ifndef WAV_H
+#define WAV_H
+
+#include <vector>
+#include <string>
+
+#include "WaveHeader.h"
+#include "IPrintable.h"
+#include "Subchunk.h"
